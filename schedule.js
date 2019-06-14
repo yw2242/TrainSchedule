faf0966cbdd3a2889c3fbea2edcd1f38a5031230
Megan Wu <meganwu1994@gmail.com>
2019-06-13 22:31:16 -0500

    added javascript for time, and changed the html type for the first train schedule

--- schedule.js
+++ schedule.js
@@ -1,25 +1,26 @@
 
     // Initialize Firebase
     var firebaseConfig = {
-        apiKey: "AIzaSyCM8512rOPTlJ2dSOSPj9kIcjphxIoIWuA",
-        authDomain: "fab4-86980.firebaseapp.com",
-        databaseURL: "https://fab4-86980.firebaseio.com",
-        projectId: "fab4-86980",
-        storageBucket: "fab4-86980.appspot.com",
-        messagingSenderId: "307656792603",
-        appId: "1:307656792603:web:746a8b645737c965"
-      };    
-
+        apiKey: "AIzaSyAT_NuXm6TXZRMfhyLJarllqjo8HcynYPc",
+        authDomain: "june-project-67f39.firebaseapp.com",
+        databaseURL: "https://june-project-67f39.firebaseio.com",
+        projectId: "june-project-67f39",
+        storageBucket: "june-project-67f39.appspot.com",
+        messagingSenderId: "468248133464",
+        appId: "1:468248133464:web:0e0b7cd2aabd9f52"
+      };
+     
+    // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
     // Create a variable to reference the database.
     var database = firebase.database();
 
-    // Initial Values
-    var name = "";
-    var dest = "";
-    var first = "";
-    var freq = "";
+    // // Initial Values
+    // var name = "";
+    // var dest = "";
+    // var first = "";
+    // var freq = "";
 
 
     // Capture Button Click
@@ -27,48 +28,88 @@
       event.preventDefault();
 
       // Grabbed values from text boxes
-      name = $("#name-input").val().trim();
-      dest = $("#dest-input").val().trim();
-      first = $("#first-input").val().trim();
-      freq = $("#freq-input").val().trim();
+      var tname = $("#name-input").val().trim();
+      var tdest = $("#dest-input").val().trim();
+      var tfirst = $("#first-input").val().trim();
+      var tfreq = $("#freq-input").val().trim();
 
-      console.log(name);
-      console.log(dest);
-      console.log(first);
-      console.log(freq);
+
 
       // Code for handling the push
-      database.ref().push({
-        name: name,
-        dest: dest,
-        first: first,
-        freq: freq,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+      var newTrain = {
+        name: tname,
+        dest: tdest,
+        first: tfirst,
+        freq: tfreq,
+      };
+
+      database.ref().push(newTrain);
+
+
+      console.log(newTrain.name);
+      console.log(newTrain.tdest);
+      console.log(newTrain.tfirst);
+      console.log(newTrain.tfreq);
+
+
+      $("#name-input").val("");
+      $("#dest-input").val("");
+      $("#first-input").val("");
+      $("#freq-input").val("");
 
     });
 
 
-    // $(document).on("click", "")
-
     // Firebase watcher .on("child_added"
-    database.ref().on("child_added", function(snapshot) {
+    database.ref().on("child_added", function(childSnapshot) {
       // storing the snapshot.val() in a variable for convenience
-      var sv = snapshot.val();
+      var tname = childSnapshot.val().name;
+      var tdest = childSnapshot.val().dest;
+      var tfirst = childSnapshot.val().first;
+      var tfreq = childSnapshot.val().freq;
 
-    //   // Console.loging the last user's data
-    //   console.log(sv.name);
-    //   console.log(sv.dest);
-    //   console.log(sv.first);
-    //   console.log(sv.freq);
+
+      console.log(tname);
+      console.log(tdest);
+      console.log(tfirst);
+      console.log(tfreq);
+
+
+      var tFrequency = tfreq;
+      var firstTimeConverted = moment(tfirst, "hh:mm").subtract(1, "years");
+      console.log(firstTimeConverted);
+
+      
+      // Current Time
+      var currentTime = moment();
+      console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+      var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+      console.log("DIFFERENCE IN TIME: " + diffTime);
+
+      // Time apart (remainder)
+      var tRemainder = diffTime % tFrequency;
+      console.log(tRemainder);
+  
+      // Minute Until Train
+      var taway = tFrequency - tRemainder;
+      console.log("MINUTES TILL TRAIN: " + taway);
+  
+      // Next Train
+      var nextTrain = moment().add(nextTrain, "minutes");
+      console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+    
 
       // Change the HTML to reflect
-      $("#name-display").text(sv.name);
-      $("#dest-display").text(sv.dest);
-      $("#first-display").text(sv.first);
-      $("#freq-display").text(sv.freq);
+      var newRow = $("<tr>").append(
+        $("<td>").text(tname),
+        $("<td>").text(tdest),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tfreq),
+        $("<td>").text(taway),
+      );
 
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
+      $("#trainTable > tbody").append(newRow);  
+
+
     });
